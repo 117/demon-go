a5e34ae22d79f123bd58d26192dca631f64f60f8
chief <admin@117.sh>
2019-09-09 03:11:20 -0400

    Update demon.go

--- demon.go
+++ demon.go
@@ -13,12 +13,15 @@ func init() {
 	logger.Formatter(func(level logger.Level, message string, vars ...interface{}) string {
 		return fmt.Sprintf("demon > %s", fmt.Sprintf(message, vars...))
 	})
+
+	// dir, err := os.UserCacheDir()
+	// make
 }
 
 func main() {
 	if len(os.Args) <= 1 {
 		command.GetCommand().Help()
-		return
+	} else {
+		command.GetCommand().Execute()
 	}
-	command.GetCommand().Execute()
 }
