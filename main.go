7bfb1cbce4de09263aff5262fd202f1469faf635
Master Chief <admin@117.sh>
2019-10-02 02:02:53 -0400

    move usage

--- main.go
+++ main.go
@@ -9,24 +9,6 @@ import (
 	"github.com/streamwithme/demon/commands"
 )
 
-const (
-	usage = ` _____   ______  __    __  ______  __   __    
-/\  __-./\  ___\/\ "-./  \/\  __ \/\ "-.\ \   
-\ \ \/\ \ \  __\\ \ \-./\ \ \ \/\ \ \ \-.  \  
- \ \____-\ \_____\ \_\ \ \_\ \_____\ \_\\"\_\ 
-  \/____/ \/_____/\/_/  \/_/\/_____/\/_/ \/_/ 
-
-Run any command or script as a daemon.
-
-demon 
-  help - show this usage
-  spawn <command(s)> - create a new daemon
-  destroy <pid|regex> - destroy a previously created daemon
-  list <?pid|regex> - list running daemons
-
-For issues https://github.com/streamwithme/demon.`
-)
-
 func init() {
 	logger.Formatter(func(level logger.Level, message string, vars ...interface{}) string {
 		return fmt.Sprintf("demon > %s", fmt.Sprintf(message, vars...))
@@ -49,5 +31,19 @@ func main() {
 		}
 	}
 
-	fmt.Println(usage)
+	fmt.Println(` _____   ______  __    __  ______  __   __    
+	/\  __-./\  ___\/\ "-./  \/\  __ \/\ "-.\ \   
+	\ \ \/\ \ \  __\\ \ \-./\ \ \ \/\ \ \ \-.  \  
+	 \ \____-\ \_____\ \_\ \ \_\ \_____\ \_\\"\_\ 
+	  \/____/ \/_____/\/_/  \/_/\/_____/\/_/ \/_/ 
+	
+	Run any command or script as a daemon.
+	
+	demon 
+	  help - show this usage
+	  spawn <command(s)> - create a new daemon
+	  destroy <pid|regex> - destroy a previously created daemon
+	  list <?pid|regex> - list running daemons
+	
+	For issues https://github.com/streamwithme/demon.`)
 }
