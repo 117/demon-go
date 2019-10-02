26c88dadeca3cf05efe85f40ad144e41e493efb2
Master Chief <admin@117.sh>
2019-10-02 02:41:53 -0400

    fix usage issue

--- main.go
+++ main.go
@@ -9,6 +9,22 @@ import (
 	"github.com/streamwithme/demon/commands"
 )
 
+const usage = ` _____   ______  __    __  ______  __   __    
+/\  __-./\  ___\/\ "-./  \/\  __ \/\ "-.\ \   
+\ \ \/\ \ \  __\\ \ \-./\ \ \ \/\ \ \ \-.  \  
+ \ \____-\ \_____\ \_\ \ \_\ \_____\ \_\\"\_\ 
+  \/____/ \/_____/\/_/  \/_/\/_____/\/_/ \/_/ 
+
+Run any command or script as a daemon.
+
+demon
+	help 			show this usage
+	spawn <command(s)>	create a new daemon
+	destroy <pid|regex>	destroy a previously created daemon
+	list <?pid|regex>	ist running daemons
+
+Have a suggestion? (https://github.com/streamwithme/demon/issues).`
+
 func init() {
 	logger.Formatter(func(level logger.Level, message string, vars ...interface{}) string {
 		return fmt.Sprintf("demon > %s", fmt.Sprintf(message, vars...))
@@ -18,33 +34,32 @@ func init() {
 func main() {
 	args := os.Args[1:]
 
-	if len(args) < 1 {
-		fmt.Println(` _____   ______  __    __  ______  __   __    
-/\  __-./\  ___\/\ "-./  \/\  __ \/\ "-.\ \   
-\ \ \/\ \ \  __\\ \ \-./\ \ \ \/\ \ \ \-.  \  
- \ \____-\ \_____\ \_\ \ \_\ \_____\ \_\\"\_\ 
-  \/____/ \/_____/\/_/  \/_/\/_____/\/_/ \/_/ 
-
-Run any command or script as a daemon.
-
-demon 
-	help - show this usage
-	spawn <command(s)> - create a new daemon
-	destroy <pid|regex> - destroy a previously created daemon
-	list <?pid|regex> - list running daemons
-
-For issues https://github.com/streamwithme/demon.`)
+	if len(args) < 1 || strings.EqualFold(args[0], "help") {
+		fmt.Println(usage)
 		return
 	}
 
-	for name, f := range map[string]func(...string){
-		"spawn":   commands.Spawn,
-		"destroy": commands.Destroy,
-		"list":    commands.List,
-	} {
-		if strings.EqualFold(name, args[0]) {
-			f(args[1:]...)
-			return
-		}
+	switch strings.ToLower(args[0]) {
+	case "s":
+	case "create":
+	case "new":
+	case "run":
+	case "spawn":
+		commands.Spawn(args[1:]...)
+		return
+	case "d":
+	case "des":
+	case "kill":
+	case "destroy":
+		commands.Destroy(args[1:]...)
+		return
+	case "l":
+	case "info":
+	case "status":
+	case "list":
+		commands.List(args[1:]...)
+		return
 	}
+
+	fmt.Println(usage)
 }
