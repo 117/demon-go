3f12f2a9a517e8d10815884811c349f5433a6761
Master Chief <admin@117.sh>
2019-10-03 15:09:37 -0400

    init

---
+++ scripts/build-and-release.sh
@@ -0,0 +1,15 @@
+repo=$(dirname "$0")
+
+if [ -z $1 ]
+then
+    echo "no version tag provided"
+else
+    rm -rf $repo/../builds
+    mkdir $repo/../builds
+    GOOS=linux GOARCH=amd64 go build -o $repo/../builds/demon-linux-$1 $repo/..
+    GOOS=darwin GOARCH=amd64 go build -o $repo/../builds/demon-darwin-$1 $repo/..
+fi
+
+git tag $1
+git push origin $1
+
