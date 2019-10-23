7e951ea80b4d7706478c3b2838fa9286161e79cc
Master Chief <16513382+117@users.noreply.github.com>
2019-10-22 20:25:56 -0400

    created an os-aware install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+REPO="117/demon"
+OS="$(tr [A-Z] [a-z] <<<$(uname))"
+TAG=$(curl --silent "https://api.github.com/repos/$REPO/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+
+if [[ $UNAME == CYGWIN* || $UNAME == MINGW* ]]; then
+    exit "Sorry your operating system is not supported."
+fi
+
+echo "--- (117/demon) Release Snapshot ---"
+echo "os            $OS"
+echo "tag           $TAG"
+
+EXECUTABLE="demon-$OS-$TAG"
+
+echo "executable    $EXECUTABLE"
+
+RELEASE_ASSET="https://github.com/$REPO/releases/download/$TAG/$EXECUTABLE"
+
+echo "--- Confirm ---"
+echo "You may be prompted for your password to make the executable."
+echo "Press any key to contine or CTRL + C to abort..."
+
+read
+
+curl -s https://api.github.com/repos/$REPO/releases/latest |
+    grep "browser_download_url.*$EXECUTABLE" |
+    cut -d : -f 2,3 |
+    tr -d \" |
+    wget -qi -
